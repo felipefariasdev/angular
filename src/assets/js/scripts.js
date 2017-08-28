759e6d0fb50621cc852eadcd8341f109dc83b617
Felipe Farias <felipe.farias@ctis.com.br>
2017-08-28 16:50:53 -0300

    import css bootstrap e font dentro do style.css

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -3,13 +3,3 @@ $(document).ready(function() {
     $('.row-offcanvas').toggleClass('active');
   });
 });
-
-$(document).ready(function () {
-  $(".nav li").removeClass("active");//this will remove the active class from
-                                     //previously active menu item
-  $('#home').addClass('active');
-  //for demo
-  //$('#demo').addClass('active');
-  //for sale
-  //$('#sale').addClass('active');
-});
